12d2b5d5493953123e888d54c1005e550e804690
Minhyeok1995 <76654360+minhyeok2487@users.noreply.github.com>
2023-05-23 22:10:02 +0900

    회원가입 로그인 개발중

---
+++ Backend/src/main/java/lostark/calendar/domain/users/User.java
@@ -0,0 +1,26 @@
+package lostark.calendar.domain.users;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+}
---
+++ Backend/src/test/java/lostark/calendar/web/UserControllerTest.java
@@ -0,0 +1,60 @@
+package lostark.calendar.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lostark.calendar.domain.users.dtos.UserJoinRequest;
+import lostark.calendar.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join() throws Exception {
+        String userName = "Minhyeok";
+        String password = "123456";
+
+        mockMvc.perform(post("/api/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(new UserJoinRequest(userName, password))))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - userName 중복")
+    void join_fail() throws Exception {
+        String userName = "Minhyeok";
+        String password = "123456";
+
+        mockMvc.perform(post("/api/users/join")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"userName\": \"" + userName + "\", \"password\": \"" + password + "\"}"))
+                .andDo(print())
+                .andExpect(status().isConflict());
+    }
+}
